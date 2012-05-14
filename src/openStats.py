a7d02e7d2c50699335ecdecdc034d10ed7559624
Sebasti?n L?pez Buritic? <Sebasti?n L?pez Buritic?@localhost>
2012-05-14 17:06:30 -0500

    faster statistics under mit license from  http://code.google.com/p/python-statlib/

---
+++ src/openStats.py
@@ -0,0 +1,33 @@
+'''
+Created on 14/05/2012
+
+@author: USUARIO
+'''
+from statlib import stats
+class statistics:
+    def __init__(self, data, name, missing):
+        self.missing= missing
+        self.Name = name
+        self.N = len(data)
+        self.suma= sum(data)
+        self.mean= stats.mean(data)
+        self.variance = stats.var(data)
+        self.stderr= stats.sterr(data)
+        self.sumsquares = stats.ss(data)
+        #self.short = stats.shellsort(data)
+        self.minimum= min(data)
+        self.maximum= max(data)
+        self.range = self.maximum-self.minimum
+        self.geomean= stats.geometricmean(data)
+        self.harmmean = stats.harmonicmean(data)
+        self.skewness= stats.skew(data)
+        self.kurtosis= stats.kurtosis(data)
+        self.median= stats.median(data)
+        self.stddev= stats.stdev(data)
+        self.samplevar= stats.samplevar(data)
+        self.variance= stats.var(data)
+        if self.mean !=0:
+            self.coeffvar= self.stddev/float(self.mean)
+        else:
+            self.coeffvar = None
+        self.mode= stats.mode(data)[1][0]
