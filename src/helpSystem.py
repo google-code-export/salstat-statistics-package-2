def2fabfee0f32870e9fff1380eaa784dfc80fff
selobu <selobu@localhost>
2012-06-29 07:34:11 -0500

    [fix] the dialog windows now displays correctly

--- src/helpSystem.py
+++ src/helpSystem.py
@@ -1,40 +1,27 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python
+#!/usr/bin/env pytho
 import wx
-from wx.html import HtmlHelpData, HtmlHelpWindow
+from wx.html import HtmlHelpWindow
 import sys
 from os import path
 
-class Navegator ( wx.Frame ):
+class Navegator ( wx.Dialog ):
     def __init__( self, parent ):
-        wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Help System",
-                            pos = wx.DefaultPosition, size = wx.Size(640,480),
+        wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = u"Help System",
+                            pos = wx.DefaultPosition, size = parent.GetClientSize(),
                             style = wx.DEFAULT_FRAME_STYLE)
-        self.path= path.abspath(path.join(path.split(sys.argv[0])[0], 'help'))
+        self.Icon= wx.GetApp().icon
         self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
         bSizer1 = wx.BoxSizer( wx.VERTICAL )
         self.m_panel1 = wx.Panel( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
-        fileName= path.join(self.path, "help.hhp")
-        ## help system ######
-        if path.isfile(fileName):
-            self.helpData= HtmlHelpData()
-            self.helpData.AddBook(fileName)
-            self.html= HtmlHelpWindow(self, -1, size= self.GetClientSize(),
-                                      helpStyle= wx.html.HF_DEFAULT_STYLE | wx.html.HF_EMBEDDED,
-                                      data= self.helpData) ## the trouble could be here
-        else:
-            self.html = wx.html.HtmlHelpWindow( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.html.HW_SCROLLBAR_AUTO)
+        self.html= HtmlHelpWindow(self, -1, 
+                                helpStyle= wx.html.HF_DEFAULT_STYLE | wx.html.HF_EMBEDDED,
+                                data= wx.GetApp().HELPDATA)
         bSizer1.Add( self.html, 1, wx.EXPAND )
-        ## help system end ##
         self.SetSizer( bSizer1 )
-        self.Fit()
         self.Layout()
         self.Centre( wx.BOTH )
-        self.Bind( wx.EVT_CLOSE, self.OnClose)
         
-    def OnClose(self,event):
-        self.Destroy()
-        event.Skip()
 
 class TestFrame( wx.Frame ):
     def __init__( self, parent ):
@@ -49,17 +36,23 @@ class TestFrame( wx.Frame ):
         self.m_button1.Bind( wx.EVT_BUTTON, self.showHelp)
         self.Bind( wx.EVT_CLOSE, self.OnClose)
 
-    def showHelp(self, event):
+    def showHelp(self, evt):
         navegador = Navegator(self)
         navegador.Show()
-        event.Skip()
+        evt.Skip()
 
-    def OnClose(self, event):
+    def OnClose(self, evt):
         self.Destroy()
-        event.Skip()
+        evt.Skip()
 
 if __name__ == '__main__':
     app= wx.App(0)
+    from wx.html import HtmlHelpData
+    import os
+    path= os.path.abspath(os.path.join(os.path.split(sys.argv[0])[0], 'help'))
+    fileName= os.path.join(path, "help.hhp")
+    app.HELPDATA= HtmlHelpData()
+    app.HELPDATA.AddBook(fileName)
     frame= TestFrame(None)
     frame.Show()
     app.MainLoop()
