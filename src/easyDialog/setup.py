f8e78a6f8c0d19f9aede596348a6f102b72722a4
sebastian lopez burititca selobu <sebastian lopez burititca selobu@gmail.com>
2013-02-18 20:32:41 -0500

    [change][easy dialog]

--- src/easyDialog/setup.py
+++ src/easyDialog/setup.py
@@ -2,4 +2,10 @@ from distutils.core import setup
 setup(name='easyDialog',
       version= '0.1',
       py_modules=['easyDialog'],
+      description='Python Dialog Creation',
+      author='Sebastian Lopez, S2 Team',
+      author_email='selobu@gmail.com',
+      url='http://code.google.com/p/salstat-statistics-package-2',
+      packages=['easyDialog',],
+      requires=['numpy (>=1.6.0)'],
       )
